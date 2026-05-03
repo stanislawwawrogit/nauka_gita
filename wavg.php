db74613a99962c8f9cbdd22ee05b9dccb0fc415f
stanislawwawrogit <wrx98375@student.wroclaw.merito.pl>
2026-05-03 14:13:55 +0200

    srednia wazona

---
+++ wavg.php
@@ -0,0 +1,35 @@
+<?php
+
+
+
+$oceny = [5, 4, 3];
+
+$wagi = [3, 2, 1];
+
+
+
+$sumaOcenRazyWagi = 0;
+
+$sumaWag = 0;
+
+
+
+for ($i = 0; $i < count($oceny); $i++) {
+
+    $sumaOcenRazyWagi += $oceny[$i] * $wagi[$i];
+
+    $sumaWag += $wagi[$i];
+
+}
+
+
+
+$sredniaWazona = $sumaOcenRazyWagi / $sumaWag;
+
+
+
+echo "Średnia ważona wynosi: " . $sredniaWazona;
+
+
+
+?>
